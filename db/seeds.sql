0412d8c049e22903b5fbdd74e3fb1fa517b87fda
Gavin56 <gavinobrien56@gmail.com>
2020-12-02 18:53:33 -0500

    Database set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name, devoured) values ("Firehouse Burger", false);
+insert into burgers (burger_name, devoured) values ("Classic American", false);
+insert into burgers (burger_name, devoured) values ("Double Bacon Cheeseburger", false);
