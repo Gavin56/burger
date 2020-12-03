ac4d52e719168e33f0dddb0e4f273fbbe183e232
Gavin56 <gavinobrien56@gmail.com>
2020-12-02 19:16:43 -0500

    Basic orm structure created.

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+const connection = require("./connection.js");
+
+let orm = {
+    selectAll: function(){},
+    insertOne: function(){},
+    updateOne: function(){},
+    deleteOne: function(){}
+};
+
+module.exports = orm;
