04275028705ae1e7d3e655e7effe8ac2a17685b1
Gavin56 <gavinobrien56@gmail.com>
2020-12-02 22:28:26 -0500

    Added selectAll and insertOne functions into the orm file.

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,47 @@
 const connection = require("./connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
 let orm = {
-    selectAll: function(){},
-    insertOne: function(){},
-    updateOne: function(){},
-    deleteOne: function(){}
+    selectAll: function (tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    updateOne: function () { },
+    deleteOne: function () { }
 };
 
 module.exports = orm;
