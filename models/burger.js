eb72f80e7bb196b1facd95e2e66898dcbf9bda58
Gavin56 <gavinobrien56@gmail.com>
2020-12-02 21:39:15 -0500

    Basic model created.

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+const orm = require("../config/orm.js");
+
+let burger = {
+    selectAll: function(){},
+    insertOne: function(){},
+    updateOne: function(){},
+    deleteOne: function(){}
+};
+
+module.exports = burger;
