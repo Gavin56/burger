bcab4bc9f229422185065ce763b0143299a7b40b
Gavin56 <gavinobrien56@gmail.com>
2020-12-02 21:45:04 -0500

    Basic controller file structure created.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+//router.get() 
+
+//router.post()
+
+//router.put()
+
+//router.delete()
+
+module.exports = router;
