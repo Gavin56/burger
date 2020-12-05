0f50ea68f1a26ae120733af4cc96e9911ed03d04
Gavin56 <gavinobrien56@gmail.com>
2020-12-04 22:14:33 -0500

    Wrote out the controller file.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,30 @@
 const connection = require("./connection.js");
 
+//===================================================================================
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+    
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string") {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+//===================================================================================
+
 function printQuestionMarks(num) {
     var arr = [];
 
@@ -40,8 +65,37 @@ let orm = {
             cb(result);
         });
     },
-    updateOne: function () { },
-    deleteOne: function () { }
+    updateOne: function (table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table; 
+
+        queryString += "SET ";
+        //Why this function?
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+     },
+    deleteOne: function (table, condition, cb) { 
+        let queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+              throw err;
+            }
+      
+            cb(result);
+          });
+    }
 };
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,62 @@
 const express = require("express");
+const cat = require("../../../../RUT-SOM-FSF-PT-09-2020-U-C/13-MVC/01-Activities/16-MVC Example(cats)/models/cat.js");
 const burger = require("../models/burger.js");
 
 const router = express.Router();
 
-//router.get() 
+//Two gets - one for the html and one for the burger data
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-//router.post()
+router.get("/burgers", function (req, res) {
+    burger.selectAll(function (data) {
+        res.json({ burgers: data });
+    });
+});
 
-//router.put()
+//Post will handle creating new burgers
+router.post("/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        //figure out what goes here...
+        // Send back the ID of the new quote
+        //res.json
+    });
+});
 
-//router.delete()
+//Put will handle changing burgers to "devoured"
+router.put("/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    //ask about this block and what it does:
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.json({ id: req.params.id });
+        }
+    });
+});
+
+router.delete("/burgers/:id", function(req,res) {
+    let condition = "id = " + req.params.id;
+
+    //why is there no need to pass the "table" into this function here? How does passing work.
+    burger.deleteOne(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
